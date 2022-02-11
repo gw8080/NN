fed59f2425bd6e172ced56117ae10915d0ac893d
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-11 20:21:07 +1100

    DB as folder with classification targets containing training data e.g DB\\mountain\\, DB\\cat\\, DB\\fish\\,

--- nn.py
+++ nn.py
@@ -3,102 +3,119 @@ import numpy as np
 import os
 import PIL
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-
 import pathlib
+import cv2
+import pyautogui
+import webbrowser
+import vlc
+import pafy
+option = input ("Do you want to: load or save the model. [load/save]? : ")
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
-
+data_dir = pathlib.Path("C:\\Users\\George\\.keras\\datasets\\DB")
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
-
 batch_size = 32
 img_height = 180
 img_width = 180
-
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-  
+data_dir,
+validation_split=0.2,
+subset="training",
+seed=123,
+image_size=(img_height, img_width),
+batch_size=batch_size)
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-  
+data_dir,
+validation_split=0.2,
+subset="validation",
+seed=123,
+image_size=(img_height, img_width),
+batch_size=batch_size)
 class_names = train_ds.class_names
 print(class_names)
-
 AUTOTUNE = tf.data.AUTOTUNE
-
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
 normalization_layer = layers.Rescaling(1./255)
-
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
-
-
 num_classes = len(class_names)
-
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+layers.Conv2D(16, 3, padding='same', activation='relu'),
+layers.MaxPooling2D(),
+layers.Conv2D(32, 3, padding='same', activation='relu'),
+layers.MaxPooling2D(),
+layers.Conv2D(64, 3, padding='same', activation='relu'),
+layers.MaxPooling2D(),
+layers.Flatten(),
+layers.Dense(128, activation='relu'),
+layers.Dense(num_classes)
 ])
-
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-              
-epochs=2
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+if option == "save":    
+    epochs=int(input("epochs:"))
+    history = model.fit(
+train_ds,
+validation_data=val_ds,
+epochs=epochs
+    )
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+if option == "load":
+    model = keras.models.load_model("my_model")
+    cap = cv2.VideoCapture(0)
+#webcam stuff
+#   while(cap.isOpened()):
+#        ret, frame = cap.read()
+#        if ret == False:
+#            break
+#        cv2.imwrite('Frame'+str(i)+'.jpg', frame)
+#        sunflower_path = 'Frame'+str(i)+'.jpg'
+#        img = tf.keras.utils.load_img(
+#        sunflower_path, target_size=(img_height, img_width)
+#        )
+#        img_array = tf.keras.utils.img_to_array(img)
+#        img_array = tf.expand_dims(img_array, 0) # Create a batch
+#        predictions = model.predict(img_array)
+#        score = tf.nn.softmax(predictions[0])
+#        print(
+#        "This image most likely belongs to {} with a {:.2f} percent confidence."
+#        .format(class_names[np.argmax(score)], 100 * np.max(score))
+#        )
+#        i += 1
+#cap.release()
+#cv2.destroyAllWindows()
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-
-sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
-
-img = tf.keras.utils.load_img(
+# take screenshot using pyautogui
+i = 0
+while(True):
+    i += 1
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image),
+                     cv2.COLOR_RGB2BGR)
+    cv2.imwrite("frame.png", image)
+    sunflower_path = 'Frame.png'
+    img = tf.keras.utils.load_img(
     sunflower_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-print(
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    print(class_names)
+    print("{}".format(score)
+    )
+    print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
+    
