a369653496983635c5a7e06b24a8f97268b8e276
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-12 19:32:30 +1100

    Add files via upload

---
+++ picPut.py
@@ -0,0 +1,21 @@
+import shutil
+import os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+mypath = "C:\\Users\\George\\Desktop\\nn\\pics\\"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
+for file in onlyfiles:
+    if file.find("_") > -1:
+        foldername = file.split("_")
+        dirname = str(foldername[0])
+        if os.path.isdir(dirname) == True:
+            print(file)
+            Path(mypath + str(file)).rename(mypath + str(foldername[0]) + "\\" + str(file))
+        if os.path.isdir(dirname) == False:
+            os.mkdir(dirname)
+            print(file)
+            Path(mypath + str(file)).rename(mypath + str(foldername[0]) + "\\" + str(file))
+            
