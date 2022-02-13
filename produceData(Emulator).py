c16532094b113191ac9bac6e33c22469d7e94b64
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-13 21:54:44 +1100

    Add files via upload

--- produceData(Emulator).py
+++ produceData(Emulator).py
@@ -8,36 +8,47 @@ import logging
 import time
 from pynput import keyboard
 i = 0
+rest = 0
 folderint = 0
 attentionThreshold = 0.01 #seconds
 tic = time.process_time()
-reset = 0
+restAmount = 1
 print("Collecting data...")
 
 
 def on_press(key):
+
     global i
-    sleep(0.001)
+    global rest
     try:
-        image = pyautogui.screenshot()
-        image = cv2.cvtColor(np.array(image),
-        cv2.COLOR_RGB2BGR)
-        dirname = str(key.char)
-        if os.path.isdir(key.char) == False:
-            os.mkdir(key.char)
-            cv2.imwrite(str(key.char) + "\\frame" + str(i) +".png", image)
-            i += 1
-        if os.path.isdir(key.char) == True:
-            cv2.imwrite(str(key.char) + "\\frame" + str(i) +".png", image)
-            i += 1
-        
+        if rest > restAmount:
+            print ("key pressed")  
+            image = pyautogui.screenshot()
+            image = cv2.cvtColor(np.array(image),
+            cv2.COLOR_RGB2BGR)
+            dirname = str(key.char)
+            if os.path.isdir(key.char) == False:
+                os.mkdir(key.char)
+                cv2.imwrite(str(key.char) + "\\frame" + str(i) +".png", image)
+                i += 1
+            if os.path.isdir(key.char) == True:
+                cv2.imwrite(str(key.char) + "\\frame" + str(i) +".png", image)
+                i += 1
+            rest = 0
+        rest += 1
+    except AttributeError:
+        print('error')
+        rest = 0
+    
+    return rest
+
+   
+def on_release(key):
+    try:
+        print ("key released")    
     except AttributeError:
         print('error')
 
-def on_release(key):
-   print ("key released")
-            
-    
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
