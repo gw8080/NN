c6a260f95ea59fa978ccbaf9863c3cd77cdf741d
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-18 12:35:50 +1100

    Add files via upload

--- AI.py
+++ AI.py
@@ -9,41 +9,15 @@ if user_inputB == "download":
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2')
     model.save_pretrained("./cached-GPT2")
-if user_inputB == "load":
-    tokenizer = GPT2Tokenizer.from_pretrained('./cached-GPT2')
+    tokenizer.save_pretrained("./cached_t-GPT2")
+if user_inputB == "exec":
     model = GPT2LMHeadModel.from_pretrained('./cached-GPT2')
+    tokenizer = GPT2Tokenizer.from_pretrained('./cached_t-GPT2')
 while(True):
     user_input = input("USER:")
-    cancel = 0
-    while(True):
-        Str = ""
-        i = 0
-        processB = word_tokenize(user_input)
-        for wordB in processB:
-            if wn.synsets(wordB):
-                scan2 = wn.synsets(wordB)[0]
-                data = list(set([w for s in scan2.closure(lambda s:s.hyponyms()) for w in s.lemma_names()]))
-                for line in data:
-                    syn_arr2 = wn.synsets(line)
-                    scan3 = syn_arr2[0].definition()
-                    process = word_tokenize("".join(scan3))
-                    for word in process:
-                        inputs = tokenizer.encode(scan3, return_tensors='pt')
-                        outputs = model.generate(
-                        inputs, max_length=50, do_sample=True, temperature=5.0
-                        )
-                        words = tokenizer.decode(outputs[0], skip_special_tokens=True)
-                        if words.find(word) != -1:
-                            i+=1
-                            if i > len(inputs)/1.5:
-                               Str = tokenizer.decode(outputs[0], skip_special_tokens=True)
-                               break
-                    if i > len(inputs)/1.5:
-                        cancel = 1
-                        break
-            if cancel == 1:
-                break
-        if cancel == 1:
-            break
-    print(Str)
-    
+    inputs = tokenizer.encode(user_input, return_tensors='pt')
+    outputs = model.generate(
+    inputs, max_length=50, do_sample=True, temperature=5.0
+    )
+    words = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
